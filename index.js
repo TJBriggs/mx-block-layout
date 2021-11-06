a2921a6858f1ed8eabe72440d52349a53fedc8fa
Thad Briggs <tbriggs@themxgroup.com>
2021-11-06 18:47:12 -0500

    Add new NPM block template

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+	defaultValues: {
+		author: 'The Mx Group',
+		category: 'mx-category',
+		slug: 'starter_block',
+		namespace: 'mx',
+		title: 'Mx Starter Block',
+		description: 'Starter block written with ESNext standard and JSX support.',
+		dashicon: 'superhero-alt',
+		textdomain: 'mx-blocks-plugin'
+	},
+	templatesPath: __dirname,
+};
